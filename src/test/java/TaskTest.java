ae99add71f9d1e4ac5e512053d36b269bae3f28e
Alena Zakirova <grachevaalek@gmail.com>
2023-11-03 11:27:24 +0300

    initial commit

---
+++ src/test/java/TaskTest.java
@@ -0,0 +1,85 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TaskTest {
+    @Test
+    public void testSimpleTaskMatches() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        boolean actual = simpleTask.matches("Позвонить");
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void testSimpleTaskMatchesNegative() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        boolean actual = simpleTask.matches("позвонить");
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    public void testEpicMatches() {
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        boolean actual = epic.matches("Яйца");
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void testEpicMatchesNegative() {
+        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic = new Epic(55, subtasks);
+
+        boolean actual = epic.matches("Макароны");
+
+        Assertions.assertFalse(actual);
+    }
+
+    @Test
+    public void testMeetingMatchesTopic() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        boolean actual = meeting.matches(" 3й ");
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void testMeetingMatchesProject() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        boolean actual = meeting.matches("Приложение ");
+
+
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void testMeetingMatchesNegative() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        boolean actual = meeting.matches("Молоко");
+
+        Assertions.assertFalse(actual);
+    }
+}
